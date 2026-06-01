4bdfadba5f70fe60e53058c81f5b5275f7f6227e
Ajay Thakare <143386761+ajay-thakare@users.noreply.github.com>
2026-06-02 05:11:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal {
 
-class Dog extends Animal {}
+	
+	constructor(species: string){
+		this.species = species;
+	}
 
-class Cat extends Animal {}
+	get species: string(){
+		return this.species;
+	}
+
+	set makeSound: void(){
+		console.log(`the ${this.species} makes a sound`);
+	}
+}
+
+class Cat extends Animal {
+	purr():void{
+		console.log('purr');
+	}
+}
+
+
+class Dog extends Animal {
+	bark(): void{
+		console.log('woof');
+	}
+}
+
 
 // Do not change the code below this line
 window.Animal = Animal;
